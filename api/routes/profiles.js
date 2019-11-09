20aec9c02e09a6600e2d734194ed6d7ccc90f448
thanhdo <thanh.do@mevry.com>
2019-11-09 22:14:16 +0700

    some endpoint for profiles

--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -93,4 +93,52 @@ router.post("/", passportAuthenticate, (req, res) => {
     }
   });
 });
+
+// get all user profile
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find() // get all
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = "There is no profile for this user";
+        return res.status(400).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// get profile by handle
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.handle = "There is no profile for this user";
+        return res.status(400).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// get profile by user id
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ id: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.handle = "There is no profile for this user";
+        return res.status(400).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 module.exports = router;
