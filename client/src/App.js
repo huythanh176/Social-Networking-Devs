4fe100be6e9a80aaeb34f122a8e45f62fefeec05
thanhdo <thanh.do@mevry.com>
2019-11-16 20:36:42 +0700

    UI and check validation Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Landing></Landing>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar></Navbar>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
